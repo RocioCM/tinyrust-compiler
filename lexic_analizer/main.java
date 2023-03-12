e353169c88d6f055ac10dedb4db2c2289d71ce4c
FacuBenito <facuben1997@gmail.com>
2023-03-12 17:49:37 -0300

    Initial Commit

---
+++ lexic_analizer/main.java
@@ -0,0 +1,5 @@
+package lexic_analizer;
+
+public class main {
+	
+}
---
+++ utils/logger.java
@@ -0,0 +1,11 @@
+class Logger {
+	public static void lexicError(String token, String lexema, String line, String col){
+		System.out.println("ERROR: LEXICO ");
+		System.out.println("| LINEA " + line + " | COLUMNA " + col + " | TOKEN " + token + " | LEXEMA " + lexema);
+	}
+
+	public static void lexicSuccess(String token, String lexema, String line, String col){
+		System.out.println("CORRECTO: LEXICO ");
+		System.out.println(token + " | " + lexema + " | LINEA " + line + " (COLUMNA " + col + ")");
+	}
+}
